d09d51d341114fabb51736129ca9fd57dd3fd5ba
ne0n666 <darthnordon@gmail.com>
2014-06-21 23:23:53 +0200

    Basics working, styling improved

--- app/js/directives.js
+++ app/js/directives.js
@@ -1,11 +1,15 @@
 'use strict';
 
-/* Directives */
-
-
 angular.module('myApp.directives', []).
+
   directive('appVersion', ['version', function(version) {
     return function(scope, elm, attrs) {
       elm.text(version);
     };
   }]);
+	directive('helpDirective', function() {
+		return {
+			restrict: 'A',
+			templateUrl:'/views/help.html'
+		};
+	})
