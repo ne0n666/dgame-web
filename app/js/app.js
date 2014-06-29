860d7198a5759bdd71cb4718bdaf9f88679de78c
ne0n666 <darthnordon@gmail.com>
2014-06-29 21:53:16 +0200

    added favicon, feedback image

--- app/js/app.js
+++ app/js/app.js
@@ -2,8 +2,6 @@
 
 angular.module('myApp', [
   'ngRoute',
-  'myApp.filters',
-  'myApp.services',
   'myApp.directives',
   'myApp.controllers'
 ]).
--- app/js/directives.js
+++ app/js/directives.js
@@ -13,6 +13,12 @@ angular.module('myApp.directives', [])
 			restrict: 'A',
 			templateUrl:'/partials/settings.html'
 		};
+	})
+	.directive('onoffDirective', function() {
+		return {
+			restrict: 'A',
+			template:'<div class="onoffswitch"><input type="checkbox" name="onoffswitch" class="onoffswitch-checkbox" id="myonoffswitch" checked><label class="onoffswitch-label" for="myonoffswitch"><span class="onoffswitch-inner"></span><span class="onoffswitch-switch"></span></label></div>'
+		};
 	});
 
 
